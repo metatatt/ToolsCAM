7dbd363fd343d2b8eacd5d028f6585ea3e519a61
metatatt <100538673+metatatt@users.noreply.github.com>
2022-03-27 14:22:17 -0700

    displayWidth, displayHeight

--- CAM_COCO.js
+++ CAM_COCO.js
@@ -80,10 +80,10 @@ function gotDetections(error, results) {
 }
 
 function setup() {
-  createCanvas(640,480);
+  createCanvas(displayWidth, displayHeight);
   //video = createCapture(VIDEO);
   video = createCapture({audio: false, video: {facingMode: {exact: "environment"}}});
-  video.size(640,480);
+  video.size(displayWidth, displayHeight);
   video.hide();
   detector.detect(video, gotDetections);
 }
