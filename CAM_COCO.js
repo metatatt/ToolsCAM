8a063ff5ed4ec377460849405b3e6cb7a1fdfc87
metatatt <100538673+metatatt@users.noreply.github.com>
2022-03-31 21:18:45 -0700

    text(object.label,

--- CAM_COCO.js
+++ CAM_COCO.js
@@ -103,7 +103,7 @@ function draw() {
         noStroke();
         fill(0);
         textSize(32);
-        text(object.label +"\n"+"prob" + object.confidence.toFixed(2), object.x + 10, object.y + 24); //object.label + " " + object.id
+        text(object.label, object.x + 10, object.y + 24); //object.label + " " + object.id
       }
       object.timer -= 2;
       if (object.timer < 0) {
